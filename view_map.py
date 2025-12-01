77353d96916043ba6ad67c55410e15ffc6a26b2f
Joseph Lundy <joseph.lundy456@gmail.com>
2025-12-01 12:42:56 -0500

    visualizes voxel grid instead of point cloud

--- view_map.py
+++ view_map.py
@@ -5,6 +5,8 @@ import pickle
 import numpy as np
 
 
+voxel_size_setting = 0.5
+
 def visualization():
     vis = o3d.visualization.Visualizer() 
     vis.create_window('Map', visible = True) 
@@ -13,10 +15,14 @@ def visualization():
 
     
     # read pcd
-    pcd_path = "maps/map_sequence_00_100_to_300.pcd"
+    pcd_path = "maps/map_sequence_00_100_to_300_moving_vehicles.pcd"
     pcd = o3d.io.read_point_cloud(pcd_path)
 
-    vis.add_geometry(pcd)
+    voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,
+                                                            voxel_size=voxel_size_setting)
+
+    vis.add_geometry(voxel_grid)
+    # vis.add_geometry(pcd)
 
     vis.run()
     vis.destroy_window()
