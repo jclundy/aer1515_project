4a150668ce8ef8aaf62666a9e8ea1d0e95b8d4c4
Joseph Lundy <joseph.lundy456@gmail.com>
2025-12-01 20:51:02 -0500

    updates evaluate_map.py to take in arguments

--- evaluate_map.py
+++ evaluate_map.py
@@ -4,27 +4,41 @@ import open3d as o3d
 import pickle
 import numpy as np
 
+import argparse
 
-voxel_size_setting = 0.5
 
 def compare_maps():
-    # vis = o3d.visualization.Visualizer() 
-    # vis.create_window('Map', visible = True) 
-    # vis.get_render_option().point_size = 1.0
-    # vis.get_render_option().background_color = np.zeros(3)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ground-truth") #"maps/map_sequence_00_100_to_300_moving_vehicles.pcd"
+    parser.add_argument("--map") #"maps/map_sequence_00_100_to_300_moving.pcd"
+    parser.add_argument("--voxel-size", default=0.5)
+
+    voxel_size_setting = 0.5
+
+    args = parser.parse_args()
+    voxel_size_setting = args.voxel_size
+    gt_pcd_path = args.ground_truth
+    est_pcd_path = args.map
+
+    gt_pcd_name = os.path.basename(gt_pcd_path)
+    gt_pcd_dir = os.path.dirname(gt_pcd_path)
+    naive_pcd_name = "naive_" + gt_pcd_name
+
+    naive_pcd_path = os.path.join(gt_pcd_dir, naive_pcd_name)
 
     print("Evaluating precision and recall with voxel size=", voxel_size_setting)
-    
+    print("Evaluating map file at: ", est_pcd_path)
+    print("Ground truth map:", gt_pcd_path)
+    print("Naive map:", naive_pcd_path)
     # read pcd
-    gt_pcd_path = "maps/map_sequence_00_100_to_300_moving_vehicles.pcd"
     gt_pcd = o3d.io.read_point_cloud(gt_pcd_path)
     gt_pcd_voxelized = gt_pcd.voxel_down_sample(voxel_size=voxel_size_setting)
 
-    naive_pcd_path = "maps/naive_map_sequence_00_100_to_300_moving_vehicles.pcd"
+
     naive_pcd = o3d.io.read_point_cloud(naive_pcd_path)
     naive_pcd_voxelized = naive_pcd.voxel_down_sample(voxel_size=voxel_size_setting)
 
-    est_pcd_path = "maps/map_sequence_00_100_to_300_moving.pcd"
     est_pcd = o3d.io.read_point_cloud(est_pcd_path)
     est_pcd_voxelized = est_pcd.voxel_down_sample(voxel_size=voxel_size_setting)
 
@@ -60,13 +74,6 @@ def compare_maps():
     RR = 1 - fp/(num_naive_voxels - num_gt_voxels)
     F1 = 2*PR*RR/(PR+RR)
     print("PR=", PR, "; RR=", RR, "; F1=",F1)
-
-
-    # vis.add_geometry(voxel_grid)
-    # # vis.add_geometry(pcd)
-
-    # vis.run()
-    # vis.destroy_window()
 
 if __name__ == "__main__":
     compare_maps()
