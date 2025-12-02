df8df81f19f7288229c95a675f84235d0bcac000
Joseph Lundy <joseph.lundy456@gmail.com>
2025-12-01 21:29:43 -0500

    copies over generate_ground_truth_map to generate_map.py

--- generate_map.py
+++ generate_map.py
@@ -1,45 +1,13 @@
-import argparse
-import numpy as np
-import open3d as o3d
-
-# Inputs:
-'''
-1) path to point clouds (.bin) format
-example: /media/joseph/7E408025407FE1F7/Datasets/Kitti/odometry/dataset/sequences/00/velodyne
-2) path to odometry file
-example: /media/joseph/7E408025407FE1F7/Datasets/Kitti/odometry/dataset/sequences/00/poses.txt
-3) Lidar to Pose base transformation:
-
-# @ Sequence's BaseToLidar info
-  ExtrinsicLiDARtoPoseBase: # this is for the KITTI (in the removert example, using SuMa poses written in the camera coord)
-      [-1.857e-03, -9.999e-01, -8.039e-03, -4.784e-03,
-       -6.481e-03,  8.0518e-03, -9.999e-01, -7.337e-02,
-        9.999e-01, -1.805e-03, -6.496e-03, -3.339e-01,
-        0.0,        0.0,        0.0,        1.0]
-  # @ If you use the lidar-itself odometry (e.g., from A-LOAM or using odometry saver https://github.com/SMRT-AIST/interactive_slam/wiki/Example2), 
-  #   use the below identity matrix, not the above KITTI pose setting (w.r.t the camera).
-  # ExtrinsicLiDARtoPoseBase: [1.0, 0.0, 0.0, 0.0, 
-  #                            0.0, 1.0, 0.0, 0.0, 
-  #                            0.0, 0.0, 1.0, 0.0, 
-  #                            0.0, 0.0, 0.0, 1.0]
-'''
-# Outputs
-'''
-PCD point cloud
-'''
 import os 
-import sys
-import time 
 import copy 
-from io import StringIO
-
-# import pypcd # for the install, use this command: python3.x (use your python ver) -m pip install --user git+https://github.com/DanielPollithy/pypcd.git
-# from pypcd import pypcd
-
 import numpy as np
 from numpy import linalg as LA
-
 import open3d as o3d
+import yaml
+import sys
+sys.path.insert(0, "../semantic-kitti-api")  # add Folder_2 path to search list
+
+from auxiliary.laserscan import LaserScan, SemLaserScan
 
 def read_bin(bin_path):
     scan = np.fromfile(bin_path, dtype=np.float32)
@@ -47,52 +15,114 @@ def read_bin(bin_path):
 
     return scan
 
+##########################
+# Parse Arguments
+##########################
+default_filter_options = ["moving", "vehicles"]
 
-# jet_table = np.load('jet_table.npy')
-# bone_table = np.load('bone_table.npy')
 
-# color_table = bone_table
-# color_table_len = color_table.shape[0]
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--sequence", default="00")
+parser.add_argument("--config") #"configs/config0.yaml"
+parser.add_argument("--visualize-only", action='store_true') #"configs/config0.yaml"
 
+args = parser.parse_args()
+map_config_file = args.config
+# open config file
+try:
+    print("Opening map config file %s" % map_config_file)
+    MAP_CONFIG = yaml.safe_load(open(map_config_file, 'r'))
+except Exception as e:
+    print(e)
+    print("Error opening yaml file.")
+    quit()
+
+if(MAP_CONFIG.get("filters") is None):
+    filter_options = []
+else:
+    filter_options = MAP_CONFIG["filters"]
 
 ##########################
 # User only consider this block
 ##########################
-sequence = "00"
-data_dir = "/media/joseph/7E408025407FE1F7/Datasets/Kitti/odometry/dataset/sequences/" + sequence + "/" # should end with / 
-node_skip = 2
+if(MAP_CONFIG.get("filters") is None):
+    sequence = args.sequence
+else:
+    sequence = MAP_CONFIG["sequence"]
 
-num_points_in_a_scan = 150000 # for reservation (save faster) // e.g., use 150000 for 128 ray lidars, 100000 for 64 ray lidars, 30000 for 16 ray lidars, if error occured, use the larger value.
+data_path = MAP_CONFIG["data_path"]
+data_dir = os.path.join(data_path, sequence)
 
-is_live_vis = False # recommend to use false 
-is_o3d_vis = True
-intensity_color_max = 200
+thres_near_removal = MAP_CONFIG["removal_thresh"] # meter (to remove platform-myself structure ghost points)
 
-is_near_removal = True
-thres_near_removal = 2 # meter (to remove platform-myself structure ghost points)
+if(MAP_CONFIG.get("skip") is None):
+    node_skip = 1
+else:
+    node_skip = MAP_CONFIG["skip"]
+
+# filter_options = ["moving", "vehicles", "ground", "sidewalk"]
+# filter_options = ["moving", "vehicles"]
+# filter_options = ["moving"]
+
 
 ##########################
 
+scan_dir = os.path.join(data_dir, "velodyne")
 
-#
-# scan_dir = data_dir + "Scans"
-# scan_dir = data_dir + "velodyne"
-scan_dir = "/media/joseph/7E408025407FE1F7/Datasets/Kitti/odometry/dataset_projected_0/sequences/00/ground_segmentation/non_ground"
 scan_files = os.listdir(scan_dir) 
 scan_files.sort()
 
-# scan_idx_range_to_stack = [0, len(scan_files)] # if you want a whole map, use [0, len(scan_files)]
-scan_idx_range_to_stack = [100, 150] # if you want a whole map, use [0, len(scan_files)]
+if(MAP_CONFIG.get("start_idx") is None):
+    startIdx = 0
+else:
+    startIdx = MAP_CONFIG["start_idx"]
 
-# pose_file = "optimized_poses.txt"
-pose_dir = "/home/joseph/catkin/scaloam_ws/src/SC-A-LOAM/utils/python/results/latest/"
-# pose_file = "poses.txt"
-# pose_file = "03_gt_tum.txt"
+if(MAP_CONFIG.get("end_idx") is None):
+    endIdx = len(scan_files)
+else:
+    endIdx = MAP_CONFIG["end_idx"]
 
-pose_file = "00_poses_kitti.txt"
+print("==========================================================")
+print("Running ground truth map generation with these parameters:")
+print("sequence", sequence)
+print("removal thresh", thres_near_removal)
+print("skip", node_skip)
+print("filter options", filter_options)
+print("start_idx", startIdx)
+print("end_idx", endIdx)
+print("==========================================================")
+
+scan_idx_range_to_stack = [startIdx, endIdx] # if you want a whole map, use [0, len(scan_files)]
+
+# pose_file = "00_poses_kitti.txt"
+# pose_dir = "/home/joseph/catkin/scaloam_ws/src/SC-A-LOAM/utils/python/results/latest/"
+pose_dir = data_dir
+pose_file = "poses.txt"
+
+# Label file
+label_dir = os.path.join(data_dir,"labels")
+
+default_config="../semantic-kitti-api/config/semantic-kitti.yaml"
+
+# open config file
+try:
+    print("Opening config file %s" % default_config)
+    CFG = yaml.safe_load(open(default_config, 'r'))
+except Exception as e:
+    print(e)
+    print("Error opening yaml file.")
+    quit()
+
+color_dict = CFG["color_map"]
+#######################################################################
+
+
+#######################################################################
 
 poses = []
-f = open(pose_dir+pose_file, 'r')
+pose_file_path = os.path.join(pose_dir, pose_file)
+f = open(pose_file_path, 'r')
 while True:
     line = f.readline()
     if not line: break
@@ -101,12 +131,15 @@ while True:
     poses.append(pose_SE3)
 f.close()
 
- # this is for KITTI 
-ExtrinsicLiDARtoPoseBase = np.asarray([-1.857e-03, -9.999e-01, -8.039e-03, -4.784e-03,
-       -6.481e-03,  8.0518e-03, -9.999e-01, -7.337e-02,
-        9.999e-01, -1.805e-03, -6.496e-03, -3.339e-01,
-        0.0,        0.0,        0.0,        1.0])
-ExtrinsicLiDARtoPoseBase = np.reshape(ExtrinsicLiDARtoPoseBase,(4,4))
+calibration_path = os.path.join(data_dir, "calib.txt")
+
+calib_data = np.loadtxt(calibration_path, delimiter=' ', dtype=str)
+transform_data = calib_data[4][1:13].astype(float).reshape((3,4))
+H_r4 = [0.0, 0.0, 0.0, 1.0]
+
+ExtrinsicLiDARtoPoseBase = np.vstack([transform_data, H_r4])
+print("ExtrinsicLiDARtoPoseBase.shape=", ExtrinsicLiDARtoPoseBase.shape)
+print("ExtrinsicLiDARtoPoseBase=", ExtrinsicLiDARtoPoseBase)
 
 #
 assert (scan_idx_range_to_stack[1] > scan_idx_range_to_stack[0])
@@ -120,25 +153,16 @@ vis.get_render_option().point_size = 1.0
 vis.get_render_option().background_color = np.zeros(3)
 
 nodes_count = 0
-pcd_combined_for_vis = o3d.geometry.PointCloud()
-pcd_combined_for_save = None
-
-# The scans from 000000.pcd should be prepared if it is not used (because below code indexing is designed in a naive way)
-
-# manually reserve memory for fast write  
-num_all_points_expected = int(num_points_in_a_scan * np.round((scan_idx_range_to_stack[1] - scan_idx_range_to_stack[0])/node_skip))
-
-np_xyz_all = np.empty([num_all_points_expected, 3])
-np_intensity_all = np.empty([num_all_points_expected, 1])
-curr_count = 0
+pcd_combined_gt = o3d.geometry.PointCloud()
+pcd_combined_naive = o3d.geometry.PointCloud()
 
 for node_idx in range(len(scan_files)):
     if(node_idx < scan_idx_range_to_stack[0] or node_idx >= scan_idx_range_to_stack[1]):
         continue
 
     nodes_count = nodes_count + 1
-    if( nodes_count % node_skip is not 0): 
-        if(node_idx is not scan_idx_range_to_stack[0]): # to ensure the vis init 
+    if( nodes_count % node_skip != 0): 
+        if(node_idx != scan_idx_range_to_stack[0]): # to ensure the vis init 
             continue
 
     print("read keyframe scan idx", node_idx)
@@ -147,119 +171,139 @@ for node_idx in range(len(scan_files)):
 
     scan_path = os.path.join(scan_dir, scan_files[node_idx])
 
-    # scan_pcd = o3d.io.read_point_cloud(scan_path)
+
+    ''' PART 1
+    Open Point cloud and Labels into SemanticLaserScan object
+    '''
+    print("scan_path=",scan_path)
+
+    scan = SemLaserScan(color_dict, project=True)
+    scan.open_scan(scan_path)
+
+    filename, file_extension = os.path.splitext(scan_files[node_idx])
+    label_path = os.path.join(label_dir, filename + '.label')
+    print("label_path=",label_path)
+    scan.open_label(label_path)
+    scan.colorize()
+
     scan_pcd = o3d.geometry.PointCloud()
+    scan_pcd_naive = o3d.geometry.PointCloud()
+    mask = None
 
+    """
+  0 : "unlabeled"
+  1 : "outlier"
+  10: "car"
+  11: "bicycle"
+  13: "bus"
+  15: "motorcycle"
+  16: "on-rails"
+  18: "truck"
+  20: "other-vehicle"
+  30: "person"
+  31: "bicyclist"
+  32: "motorcyclist"
+  40: "road"
+  44: "parking"
+  48: "sidewalk"
+  49: "other-ground"
+  50: "building"
+  51: "fence"
+  52: "other-structure"
+  60: "lane-marking"
+  70: "vegetation"
+  71: "trunk"
+  72: "terrain"
+  80: "pole"
+  81: "traffic-sign"
+  99: "other-object"
+  252: "moving-car"
+  253: "moving-bicyclist"
+  254: "moving-person"
+  255: "moving-motorcyclist"
+  256: "moving-on-rails"
+  257: "moving-bus"
+  258: "moving-truck"
+  259: "moving-other-vehicle"
+    """
+    ''' PART 2a
+    Filter based on point label
+    '''
+    mask = scan.sem_label != 1
+    if("moving" in filter_options):
+        mask &= (scan.sem_label < 252)
+    if("vehicles" in filter_options):
+        m1 = scan.sem_label < 10
+        m2 = scan.sem_label > 32
+        m3 = scan.sem_label < 252
+        mask = (m1 | (m2 & m3)) & mask
+    if("ground" in filter_options):
+        mask &= (scan.sem_label != 40) # road
+        mask &= (scan.sem_label != 49) # other ground
+        mask &= (scan.sem_label != 60) # lane markings
+    if("sidewalk" in filter_options):
+        mask &= (scan.sem_label != 48)
 
-    filename, file_extension = os.path.splitext(scan_path)
-    if(file_extension == ".bin"):
-        pointcloud_data = read_bin(scan_path)
-        scan_pcd.points = o3d.utility.Vector3dVector(pointcloud_data[:, :3])
-    else:
-        scan_pcd = o3d.io.read_point_cloud(scan_path)
+    scan_pcd.points = o3d.utility.Vector3dVector(scan.points[mask])
+    scan_pcd.colors = o3d.utility.Vector3dVector(scan.sem_label_color[mask])
 
     scan_xyz_local = copy.deepcopy(np.asarray(scan_pcd.points))
 
-    
-    
-    # scan_pypcd_with_intensity = None
-
-    # filename, file_extension = os.path.splitext(scan_path)
-    # if(file_extension == ".bin"):
-    #     pointcloud = read_bin(scan_path)
-    #     # read pcd
-    #     # scan_pypcd_with_intensity = pypcd.PointCloud.from_array(pointcloud)
-    #     # scan_pypcd_with_intensity = pypcd.PointCloud.from_array(pointcloud)
-
-    #     scan_pypcd_with_intensity = pypcd.make_xyz_rgb_point_cloud(pointcloud)
-    
-    #     # pcd = o3d.geometry.PointCloud()
-    #     # pcd.points = o3d.utility.Vector3dVector(pointcloud[:, :3])
-    # else:
-    #     scan_pypcd_with_intensity = pypcd.PointCloud.from_path(scan_path)
-    
-
- 
-    scan_intensity = np.ones((len(scan_pcd.points)))
-    # scan_intensity_colors_idx = np.round( (color_table_len-1) * np.minimum( 1, np.maximum(0, scan_intensity / intensity_color_max) ) )
-    # scan_intensity_colors = color_table[scan_intensity_colors_idx.astype(int)]
-
     scan_pcd_global = scan_pcd.transform(ExtrinsicLiDARtoPoseBase)
     scan_pcd_global = scan_pcd.transform(scan_pose) # global coord, note that this is not deepcopy
-    # scan_pcd_global.colors = o3d.utility.Vector3dVector(scan_intensity_colors)
-    scan_pcd_global.colors = o3d.utility.Vector3dVector(np.ones((len(scan_pcd.points), 3)))
-    scan_xyz = np.asarray(scan_pcd_global.points)
 
-    scan_intensity = np.expand_dims(scan_intensity, axis=1) 
-    scan_ranges = LA.norm(scan_xyz_local, axis=1)
 
-    if(is_near_removal):
+    ''' PART 2b
+    Filter points too close
+    '''
+    if(thres_near_removal is not None):
+        scan_ranges = LA.norm(scan_xyz_local, axis=1)
         eff_idxes = np.where (scan_ranges > thres_near_removal)
-        scan_xyz = scan_xyz[eff_idxes[0], :]
-        scan_intensity = scan_intensity[eff_idxes[0], :]
-
         scan_pcd_global = scan_pcd_global.select_by_index(eff_idxes[0])
 
-    if(is_o3d_vis):
-        reduced_scan = scan_pcd_global.voxel_down_sample(voxel_size=0.1)
-        pcd_combined_for_vis += reduced_scan # open3d pointcloud class append is fast 
+        points_removed = len(scan_ranges) - len(eff_idxes[0])
+        print("number of points removed by range check:", points_removed)
 
-    if is_live_vis:
-        if(node_idx is scan_idx_range_to_stack[0]): # to ensure the vis init 
-            vis.add_geometry(pcd_combined_for_vis) 
+    ''' PART 3
+    Voxel-based downsampling
+    '''
+    reduced_scan = scan_pcd_global.voxel_down_sample(voxel_size=0.1)
+    pcd_combined_gt += reduced_scan # open3d pointcloud class append is fast
 
-        # vis.update_geometry(pcd_combined_for_vis)
-        # vis.poll_events()
-        # vis.update_renderer()
-
-    # save 
-    np_xyz_all[curr_count:curr_count + scan_xyz.shape[0], :] = scan_xyz
-    np_intensity_all[curr_count:curr_count + scan_xyz.shape[0], :] = scan_intensity
-
-    curr_count = curr_count + scan_xyz.shape[0]
-    print(curr_count)
+    scan_pcd_naive.points = o3d.utility.Vector3dVector(scan.points)
+    scan_pcd_naive.colors = o3d.utility.Vector3dVector(scan.sem_label_color)
+    pcd_combined_naive += scan_pcd_naive.transform(ExtrinsicLiDARtoPoseBase).transform(scan_pose).voxel_down_sample(voxel_size=0.1)
  
-#
-# if(is_o3d_vis):
-# print('voxelization')
-# voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd_combined_for_vis,
-#                                                             voxel_size=0.05)
-print("Final downsampling")
-pcd_combined_for_vis = pcd_combined_for_vis.voxel_down_sample(voxel_size=0.1)
 
-vis.add_geometry(pcd_combined_for_vis)
-# print("draw the merged map.")
-# vis.add_geometry(pcd_combined_for_vis)
+print("Final downsampling")
+pcd_combined_gt = pcd_combined_gt.voxel_down_sample(voxel_size=0.1)
+pcd_combined_naive = pcd_combined_naive.voxel_down_sample(voxel_size=0.1)
+
+print("GT cloud num points: ", len(pcd_combined_gt.points))
+print("Naive cloud num points: ", len(pcd_combined_naive.points))
+
+vis.add_geometry(pcd_combined_gt)
+
 vis.run()
 vis.destroy_window()
 
-# # save ply having intensity
-# np_xyz_all = np_xyz_all[0:curr_count, :]
-# np_intensity_all = np_intensity_all[0:curr_count, :]
+vis.create_window('Map', visible = True)
+vis.get_render_option().point_size = 1.0
+vis.get_render_option().background_color = np.zeros(3)
+vis.add_geometry(pcd_combined_naive)
 
-# np_xyzi_all = np.hstack( (np_xyz_all, np_intensity_all) )
-# xyzi = make_xyzi_point_cloud(np_xyzi_all)
+vis.run()
+vis.destroy_window()
 
-# map_name = data_dir + "map_" + str(scan_idx_range_to_stack[0]) + "_to_" + str(scan_idx_range_to_stack[1]) + "_with_intensity.pcd"
-# xyzi.save_pcd(map_name, compression='binary_compressed')
-# print("intensity map is save (path:", map_name, ")")
+if(args.visualize_only != True):
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    map_dir = os.path.join(cur_dir, "maps")
+    map_name = "map_" + "sequence_" + sequence + "_" + str(scan_idx_range_to_stack[0]) + "_to_" + str(scan_idx_range_to_stack[1]) + "_" + "_".join(filter_options) + ".pcd"
+    map_path = os.path.join(map_dir, map_name)
+    o3d.io.write_point_cloud(map_path, pcd_combined_gt)
+    print("the map is save to:", map_path, ")")
 
-# save rgb colored points 
-# map_name = data_dir + "map_" + str(scan_idx_range_to_stack[0]) + "_to_" + str(scan_idx_range_to_stack[1]) + ".pcd"
-# o3d.io.write_point_cloud(map_name, pcd_combined_for_vis)
-# print("the map is save (path:", map_name, ")")
-
-
-'''
-clouds_list = [np.load(cloud_file).reshape(-1, args.point_dims) for cloud_file in args.clouds_file_list]
-
-
-input_pkl_path = os.path.join(DATASET_DIREC_PATH, args.bbox_file_name + ".pkl") # write input pkl path
-start_idx = 0
-
-ground_heights_path = os.path.join(DATASET_DIREC_PATH, "ground_heights.txt")
-
-with open(input_pkl_path,"rb") as fr:
-    results = pickle.load(fr) # load pickle file
-
-'''
+    naive_map_name = "naive_" + map_name
+    naive_path = os.path.join(map_dir, naive_map_name)
+    o3d.io.write_point_cloud(naive_path, pcd_combined_naive)
+    print("the naive acculmulated map is saved to ", naive_path)
