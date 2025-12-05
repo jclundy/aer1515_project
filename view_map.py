95cbdd7781dd1b26dc725e6e2fc910eac29d3bd2
Joseph Lundy <joseph.lundy456@gmail.com>
2025-12-04 22:39:55 -0500

    updates visualize map to show both pcd and voxel grid

--- view_map.py
+++ view_map.py
@@ -3,11 +3,23 @@ import glob
 import open3d as o3d
 import pickle
 import numpy as np
+import argparse
 
 
-voxel_size_setting = 0.5
+
+
 
 def visualization():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pcd")  # "maps/map_sequence_00_100_to_300_moving_vehicles.pcd"
+    parser.add_argument("--voxel-size", default=0.5)
+
+    args = parser.parse_args()
+    voxel_size_setting = args.voxel_size
+    pcd_path = args.pcd
+
+
     vis = o3d.visualization.Visualizer() 
     vis.create_window('Map', visible = True) 
     vis.get_render_option().point_size = 1.0
@@ -15,17 +27,25 @@ def visualization():
 
     
     # read pcd
-    pcd_path = "maps/map_sequence_00_100_to_300_moving_vehicles.pcd"
     pcd = o3d.io.read_point_cloud(pcd_path)
 
-    voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,
-                                                            voxel_size=voxel_size_setting)
 
-    vis.add_geometry(voxel_grid)
-    # vis.add_geometry(pcd)
+    vis.create_window('Map', visible = True)
+    vis.get_render_option().point_size = 1.0
+    vis.get_render_option().background_color = np.zeros(3)
+    vis.add_geometry(pcd)
 
     vis.run()
     vis.destroy_window()
+
+    voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd,
+                                                            voxel_size=voxel_size_setting)
+    vis.add_geometry(voxel_grid)
+    vis.run()
+    vis.destroy_window()
+
+
+
 
 if __name__ == "__main__":
     visualization()
